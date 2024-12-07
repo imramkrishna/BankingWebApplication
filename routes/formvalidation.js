8ba5f4b8c1edcd136f117fdb1e95ea65118d04f4
Ram Krishna Yadav <itsramky234@gmail.com>
2024-12-07 22:07:06 +0545

    Upgraded formvalidation.js

--- routes/formvalidation.js
+++ routes/formvalidation.js
@@ -2,14 +2,6 @@ import express from "express";
 import mongoose from "mongoose";
 import User from "../models/formdata.js"
 
-//Connecting to database
-
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch(err => {
-    console.error("Error connecting to MongoDB:", err);
-    process.exit(1); // Exit the process if connection fails
-  });
 
 
 //Sending post request using express router :
