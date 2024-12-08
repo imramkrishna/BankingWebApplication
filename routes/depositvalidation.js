73259490accf6c7bdcb50252764c164c16f842e1
Ram Krishna Yadav <itsramky234@gmail.com>
2024-12-08 19:58:39 +0545

    Deposit Withdraw Issue Solved

--- routes/depositvalidation.js
+++ routes/depositvalidation.js
@@ -3,6 +3,20 @@ import express from "express";
 
 import User from "../models/formdata.js";
 
+
+
+mongoose.connect('mongodb://localhost:27017/BankingDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('Error connecting to MongoDB', err);
+});
+
+
 const router=express.Router();  
 
 router.post("/",async (req,res)=>{
--- routes/withdrawValidation.js
+++ routes/withdrawValidation.js
@@ -1,5 +1,18 @@
 import User from "../models/formdata.js";
 import router from "./registration.js";
+import mongoose from 'mongoose';
+
+
+mongoose.connect('mongodb://localhost:27017/BankingDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('Error connecting to MongoDB', err);
+});
 
      
 
